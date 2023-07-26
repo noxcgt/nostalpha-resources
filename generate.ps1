ae148d748bbae6170f3ab399e576ce0380915d05
mudkipdev <mudkip@mudkip.dev>
2023-07-26 08:12:15 -0600

    updated script

--- generate.ps1
+++ generate.ps1
@@ -5,14 +5,16 @@ $output = @"
 <ResourceList>
     <Name>MinecraftResources</Name>`n
 "@
+$counter = 0
 
 Get-ChildItem -Recurse -Include *.ogg, *.mus -File | ForEach-Object {
     $output += "    <Contents>`n"
     $output += "        <Key>$((Resolve-Path $_ -Relative).Substring(2).Replace("\", "/"))</Key>`n"
     $output += "        <Size>$($_.Length)</Size>`n"
     $output += "    </Contents>`n"
+    $counter += 1
 }
 
 $output += "</ResourceList>"
-
 $output | Out-File index.xml
+Write-Output "The XML file has been generated with $($counter) resources."
---
+++ generate.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+output="<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<ResourceList>\n    <Name>MinecraftResources</Name>\n"
+counter=0
+
+find . -type f \( -name "*.ogg" -o -name "*.mus" \) | while read -r file; do
+    relative_path="${file:2}"
+    size=$(stat -c %s "$file")
+
+    output+="    <Contents>\n"
+    output+="        <Key>${relative_path//\\/\/}</Key>\n"
+    output+="        <Size>${size}</Size>\n"
+    output+="    </Contents>\n"
+
+    ((counter++))
+done
+
+output+="</ResourceList>"
+echo -e "$output" > index.xml
+echo "The XML file has been generated with ${counter} resources."
